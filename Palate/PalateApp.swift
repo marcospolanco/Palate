60f47ad78616abfd4d1e739999b57ede7e07d11f
Marcos Polanco <mjpolanco@gmail.com>
2023-06-03 14:09:58 -0700

    Initial Commit

---
+++ Palate/PalateApp.swift
@@ -0,0 +1,20 @@
+//
+//  PalateApp.swift
+//  Palate
+//
+//  Created by Marco’s Polanco on 6/3/23.
+//
+
+import SwiftUI
+
+@main
+struct PalateApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
